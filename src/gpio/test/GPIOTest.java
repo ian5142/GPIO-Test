9df6c4827e6ebe9e580159f43ba873618e35850c
ian5142 <ian@birdisland.net>
2016-11-28 12:30:39 -0400

    Tested, changed timers to 1 sec for sleep.

--- src/gpio/test/GPIOTest.java
+++ src/gpio/test/GPIOTest.java
@@ -22,36 +22,36 @@ public class GPIOTest {
         final GpioController gpio = GpioFactory.getInstance();
 
         // provision gpio pin #01 as an output pin and turn on
-        final GpioPinDigitalOutput pin = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_01, "MyLED", PinState.HIGH);
+        final GpioPinDigitalOutput pin01 = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_01, "MyLED", PinState.HIGH);
 
         // set shutdown state for this pin
-        pin.setShutdownOptions(true, PinState.LOW);
+        pin01.setShutdownOptions(true, PinState.LOW);
 
         System.out.println("--> GPIO state should be: ON");
 
-        Thread.sleep(5000);
+        Thread.sleep(1000);
 
         // turn off gpio pin #01
-        pin.low();
+        pin01.low();
         System.out.println("--> GPIO state should be: OFF");
 
-        Thread.sleep(5000);
+        Thread.sleep(1000);
 
         // toggle the current state of gpio pin #01 (should turn on)
-        pin.toggle();
+        pin01.toggle();
         System.out.println("--> GPIO state should be: ON");
 
-        Thread.sleep(5000);
+        Thread.sleep(1000);
 
         // toggle the current state of gpio pin #01  (should turn off)
-        pin.toggle();
+        pin01.toggle();
         System.out.println("--> GPIO state should be: OFF");
 
-        Thread.sleep(5000);
+        Thread.sleep(1000);
 
         // turn on gpio pin #01 for 1 second and then off
         System.out.println("--> GPIO state should be: ON for only 1 second");
-        pin.pulse(1000, true); // set second argument to 'true' use a blocking call
+        pin01.pulse(1000, true); // set second argument to 'true' use a blocking call
 
         // stop all GPIO activity/threads by shutting down the GPIO controller
         // (this method will forcefully shutdown all GPIO monitoring threads and scheduled tasks)
